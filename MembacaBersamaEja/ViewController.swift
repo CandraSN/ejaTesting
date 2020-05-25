0053ddaa6ce39ed1d4c55912dcb3131a188f3482
William Sebastian <chenyusen1999@gmail.com>
2020-05-25 20:50:37 +0700

    Storyboard hampir finish

---
+++ MembacaBersamaEja/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MembacaBersamaEja
+//
+//  Created by William Sebastian Thedja on 24/05/20.
+//  Copyright © 2020 William Sebastian Thedja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
